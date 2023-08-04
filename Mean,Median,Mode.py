50fc5915e4c1b2fb22446970b62b5e43b3859b57
seviloksuz <115875660+seviloksuz@users.noreply.github.com>
2023-08-04 12:01:35 +0300

    code added

--- Mean,Median,Mode.py
+++ Mean,Median,Mode.py
@@ -4,11 +4,23 @@ for i in range(dim):
     a=float(input("Enter the number:"))
     list.append(a)
 #Mean
-mean=sum(list)/len(list)  
+mean=sum(list)/len(list)
+print("Mean=",mean)  
 #Median
 if(dim%2==0):
-    median=(list[dim/2]+list[dim/2+1])/2
+    median=(list[(int)(dim/2-1)]+list[(int)(dim/2)])/2
 else:
-    mediam=list[(dim+1)/2]
+    median=list[(int)((dim+1)/2)]
+print("Median=",median)
 #Mode
+frequency = {} #dict
+for i in list:
+    frequency.setdefault(i, 0)
+    frequency[i]+=1
+
+frequent = max(frequency.values())
+for i, j in frequency.items():
+    if j == frequent:
+        mode = i
+        print("Mode=",mode)
 
